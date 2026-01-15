608a1b3db3493955d1e47b925102140d964c3c87
printnick3D <printnick3d@yandex.ru>
2026-01-16 00:09:22 +0300

    Create data.materials.js

---
+++ js/data.materials.js
@@ -0,0 +1,5 @@
+export const materials = {
+  PLA: { price: 600, colors: ['Белый', 'Чёрный', 'Серый'] },
+  PETG: { price: 750, colors: ['Прозрачный', 'Чёрный'] },
+  ABS: { price: 900, colors: ['Белый', 'Чёрный'] }
+};
